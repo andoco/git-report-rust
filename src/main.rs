1b179727f463f9f142461df51b1be193122b5957
Andrew O'Connor <andrew.oconnor@gmail.com>
2021-06-20 10:57:18 +0100

    Replace pattern matching with and_then and map_or_else

--- src/main.rs
+++ src/main.rs
@@ -14,21 +14,17 @@ fn main() -> Result<(), Box<dyn Error>> {
         if path.is_dir() {
             let path = path.to_str().ok_or("Not a valid path")?;
 
-            match Repository::open(path) {
-                Ok(repo) => {
-                    if is_dirty(repo)? {
-                        println!("{} is dirty", path);
-                    }
-                }
-                Err(err) => println!("{} could not open with error: {}", path, err),
-            }
+            let _ = Repository::open(path).and_then(is_dirty).map_or_else(
+                |err| println!("{} ERROR {}", path, err),
+                |dirty| print_report(path, dirty),
+            );
         }
     }
 
     Ok(())
 }
 
-fn is_dirty(repo: Repository) -> Result<bool, Box<dyn Error>> {
+fn is_dirty(repo: Repository) -> Result<bool, git2::Error> {
     let ok_statuses = [Status::CURRENT, Status::IGNORED];
     let statuses = repo.statuses(None)?;
 
@@ -37,4 +33,12 @@ fn is_dirty(repo: Repository) -> Result<bool, Box<dyn Error>> {
         .any(|entry| !ok_statuses.contains(&entry.status()));
 
     Ok(result)
+}
+
+fn print_report(path: &str, dirty: bool) {
+    if dirty {
+        println!("{} is DIRTY", path);
+    } else {
+        println!("{} is CLEAN", path);
+    }
 }
