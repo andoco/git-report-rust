04df162d17642b7dd957f9ccb0eaeb1ac5d7f9c3
Andrew O'Connor <andrew.oconnor@gmail.com>
2021-06-24 16:47:47 +0100

    Evaluate a RepoStatus from all git repo statuses

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 use std::env;
 
-use git2::{Repository, Status};
+use git2::{ErrorCode, Repository, Status};
 use std::{error::Error, fs};
+
+#[derive(Debug, PartialEq)]
+enum RepoStatus {
+    Clean,
+    Changed,
+    NotRepo,
+    Error(String),
+}
+
+impl std::fmt::Display for RepoStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Clean => write!(f, "Clean"),
+            Self::Changed => write!(f, "Changed"),
+            Self::NotRepo => write!(f, "Not a repo"),
+            Self::Error(e) => write!(f, "Error: {}", e),
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -14,31 +33,83 @@ fn main() -> Result<(), Box<dyn Error>> {
         if path.is_dir() {
             let path = path.to_str().ok_or("Not a valid path")?;
 
-            let _ = Repository::open(path).and_then(is_dirty).map_or_else(
-                |err| println!("{} ERROR {}", path, err),
-                |dirty| print_report(path, dirty),
-            );
+            let status = Repository::open(path)
+                .map_or_else(map_git_error_to_repo_status, get_repo_report_status);
+
+            println!("{} {}", path, status);
         }
     }
 
     Ok(())
 }
 
-fn is_dirty(repo: Repository) -> Result<bool, git2::Error> {
-    let ok_statuses = [Status::CURRENT, Status::IGNORED];
-    let statuses = repo.statuses(None)?;
+fn get_repo_report_status(repo: Repository) -> RepoStatus {
+    match repo.statuses(None) {
+        Ok(statuses) => {
+            let mut report_statuses = statuses
+                .iter()
+                .map(|s| s.status())
+                .map(|s| map_git_status_to_report_status(&s));
 
-    let result = statuses
-        .iter()
-        .any(|entry| !ok_statuses.contains(&entry.status()));
+            if report_statuses.any(|s| s == RepoStatus::Changed) {
+                return RepoStatus::Changed;
+            }
 
-    Ok(result)
+            return RepoStatus::Clean;
+        }
+        Err(e) => RepoStatus::Error(e.to_string()),
+    }
 }
 
-fn print_report(path: &str, dirty: bool) {
-    if dirty {
-        println!("{} is DIRTY", path);
-    } else {
-        println!("{} is CLEAN", path);
+fn map_git_error_to_repo_status(err: git2::Error) -> RepoStatus {
+    if err.code() == ErrorCode::NotFound {
+        return RepoStatus::NotRepo;
+    }
+    RepoStatus::Error(err.to_string())
+}
+
+fn map_git_status_to_report_status(status: &Status) -> RepoStatus {
+    let changed_mask = Status::INDEX_NEW
+        | Status::INDEX_MODIFIED
+        | Status::INDEX_DELETED
+        | Status::INDEX_RENAMED
+        | Status::INDEX_TYPECHANGE
+        | Status::WT_NEW
+        | Status::WT_MODIFIED
+        | Status::WT_DELETED
+        | Status::WT_RENAMED
+        | Status::WT_TYPECHANGE;
+
+    let has_changes = *status & changed_mask == *status;
+
+    if has_changes {
+        return RepoStatus::Changed;
+    }
+
+    return RepoStatus::Clean;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_map_git_status_to_report_status() {
+        let expected_changed_statuses = [
+            Status::INDEX_NEW,
+            Status::INDEX_MODIFIED,
+            Status::INDEX_DELETED,
+            Status::INDEX_RENAMED,
+            Status::INDEX_TYPECHANGE,
+            Status::WT_NEW,
+            Status::WT_MODIFIED,
+            Status::WT_DELETED,
+            Status::WT_RENAMED,
+            Status::WT_TYPECHANGE,
+        ];
+
+        for status in expected_changed_statuses.iter() {
+            assert_eq!(RepoStatus::Changed, map_git_status_to_report_status(status))
+        }
     }
 }
