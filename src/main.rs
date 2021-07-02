89a429ffee6560ded44ed6c6d4b26b144769d459
Andrew O'Connor <andrew.oconnor@gmail.com>
2021-07-02 16:39:18 +0100

    Reporting on branch status

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use std::env;
+use std::{collections::HashMap, env};
 
 use ansi_term::Colour;
-use git2::{ErrorCode, Repository, Status};
+use git2::{BranchType, ErrorCode, Repository, Status};
 use std::{error::Error, fs};
+
+#[derive(Debug)]
+struct RepoReport {
+    repo_status: RepoStatus,
+    branch_status: HashMap<String, BranchStatus>,
+}
 
 #[derive(Debug, PartialEq)]
 enum RepoStatus {
     Clean,
     Changed,
+    Unpushed,
     NotRepo,
+    Error(String),
+}
+
+#[derive(Debug)]
+enum BranchStatus {
+    Current,
+    NoUpstream,
+    Ahead,
     Error(String),
 }
 
@@ -17,6 +32,7 @@ impl std::fmt::Display for RepoStatus {
         match self {
             Self::Clean => write!(f, "Clean"),
             Self::Changed => write!(f, "Changed"),
+            Self::Unpushed => write!(f, "Unpushed"),
             Self::NotRepo => write!(f, "Not a repo"),
             Self::Error(e) => write!(f, "Error: {}", e),
         }
@@ -31,38 +47,43 @@ fn main() -> Result<(), Box<dyn Error>> {
         None => ".",
     };
 
-    let mut statuses: Vec<(String, RepoStatus)> = vec![];
-
     for entry in fs::read_dir(root_path)? {
         let entry = entry?;
         let path = entry.path();
 
         if path.is_dir() {
             let path = path.to_str().ok_or("Not a valid path")?;
-
-            let status = Repository::open(path)
-                .map_or_else(map_git_error_to_repo_status, get_repo_report_status);
-
-            statuses.push((path.to_string(), status));
+            let report = report_on_repo(path);
+            print_repo_report(path, report, 60);
         }
     }
-
-    let max_length = statuses.iter().map(|(p, _)| p.len()).max().unwrap();
-
-    statuses
-        .iter()
-        .for_each(|(p, s)| print_status(p, s, max_length));
 
     Ok(())
 }
 
-fn print_status(path: &String, status: &RepoStatus, column_size: usize) {
-    let to_print = match status {
-        RepoStatus::Clean => Colour::Green.paint("Clean"),
-        RepoStatus::Changed => Colour::Purple.paint("Changed"),
-        RepoStatus::NotRepo => Colour::Yellow.paint("Not a repo"),
-        RepoStatus::Error(e) => Colour::Red.paint(format!("Error {}", e)),
+fn report_on_repo(path: &str) -> Result<RepoReport, Box<dyn Error>> {
+    let repo = &Repository::open(path)?;
+    let status = get_repo_status(repo);
+    let branches = get_branch_status(repo)?;
+
+    Ok(RepoReport {
+        repo_status: status,
+        branch_status: branches,
+    })
+}
+
+fn print_repo_report(path: &str, report: Result<RepoReport, Box<dyn Error>>, column_size: usize) {
+    let to_print = match report {
+        Ok(report) => match report.repo_status {
+            RepoStatus::Clean => Colour::Green.paint("Clean"),
+            RepoStatus::Changed => Colour::Purple.paint("Changed"),
+            RepoStatus::Unpushed => Colour::Purple.paint("Not pushed"),
+            RepoStatus::NotRepo => Colour::Yellow.paint("Not a repo"),
+            RepoStatus::Error(e) => Colour::Red.paint(format!("Error {}", e)),
+        },
+        Err(e) => Colour::Red.paint(format!("Error {}", e)),
     };
+
     println!(
         "{}{: <width$}{}",
         path,
@@ -72,7 +93,7 @@ fn print_status(path: &String, status: &RepoStatus, column_size: usize) {
     );
 }
 
-fn get_repo_report_status(repo: Repository) -> RepoStatus {
+fn get_repo_status(repo: &Repository) -> RepoStatus {
     match repo.statuses(None) {
         Ok(statuses) => {
             let mut report_statuses = statuses
@@ -88,6 +109,44 @@ fn get_repo_report_status(repo: Repository) -> RepoStatus {
         }
         Err(e) => RepoStatus::Error(e.to_string()),
     }
+}
+
+fn get_branch_status(repo: &Repository) -> Result<HashMap<String, BranchStatus>, Box<dyn Error>> {
+    let mut branch_changes = HashMap::<String, BranchStatus>::new();
+
+    match repo.branches(Some(BranchType::Local)) {
+        Ok(branches) => {
+            for (b, _) in branches.map(|x| x.unwrap()) {
+                let branch_name = String::from(b.name()?.ok_or("could not get branch name")?);
+
+                if b.upstream().is_err() {
+                    branch_changes.insert(branch_name, BranchStatus::NoUpstream);
+                    continue;
+                }
+
+                let local_oid = repo.refname_to_id(b.get().name().ok_or("invalid ref name")?)?;
+                let upstream_oid =
+                    repo.refname_to_id(b.upstream()?.get().name().ok_or("invalid ref name")?)?;
+
+                let ahead_behind = repo.graph_ahead_behind(local_oid, upstream_oid);
+                let branch_status = match ahead_behind {
+                    Ok((ahead, _)) => {
+                        if ahead > 0 {
+                            BranchStatus::Ahead
+                        } else {
+                            BranchStatus::Current
+                        }
+                    }
+                    Err(e) => BranchStatus::Error(e.to_string()),
+                };
+
+                branch_changes.insert(branch_name, branch_status);
+            }
+        }
+        Err(e) => panic!("{}", e),
+    }
+
+    Ok(branch_changes)
 }
 
 fn map_git_error_to_repo_status(err: git2::Error) -> RepoStatus {
